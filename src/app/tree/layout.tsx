acad2c02e825d014e0a692d6489cd69596325c6b
subeend <happysong98@gmail.com>
2025-12-17 03:00:15 +0900

    fix: 박스 위치 수정

--- src/app/tree/layout.tsx
+++ src/app/tree/layout.tsx
@@ -5,15 +5,13 @@ import AppHeightSetter from '@/src/components/AppHeightSetter';
 export default function TreeLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="w-full h-[var(--app-height)] flex justify-center bg-transparent">
-      <AppHeightSetter />
-
       <div className="relative w-full max-w-[414px] h-[var(--app-height)] bg-transparent overflow-hidden">
-        <Header />
+        <AppHeightSetter />
 
-        {/* ✅ page가 화면을 책임 */}
+        <Header />
         <div className="relative w-full mt-[56px] h-[calc(var(--app-height)-56px)]">{children}</div>
 
-        <div className="fixed top-[calc(8px+env(safe-area-inset-top))] right-2 z-[9999]">
+        <div className="absolute top-[calc(8px+env(safe-area-inset-top))] right-2 z-[9999]">
           <MailboxButton />
         </div>
       </div>
